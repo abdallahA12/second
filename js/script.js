af0857dbfd398c584ad5214e496669c70c1ed5a1
Adam Abdallah <khalizatelli@gmail.com>
2019-10-28 15:43:24 +0300

    updated html

--- js/script.js
+++ js/script.js
@@ -2,5 +2,4 @@ function submtion(){
     var day = parseInt(document.getElementById("dob").value);
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
-
 }
